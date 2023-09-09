8ff509fb4992c2acd3b64df7ac73b043407bc797
jackdegen <jackbdegen@gmail.com>
2023-09-09 19:07:46 -0400

    data scraping complete, now need to format correct iteration style

---
+++ src/_info.py
@@ -0,0 +1,24 @@
+DATA_STATS = [
+    'player', # within href
+    'team',
+    'pass_cmp',
+    'pass_att',
+    'pass_yds',
+    'pass_td',
+    'pass_int',
+    'pass_sacked',
+    'pass_sacked_yds',
+    'pass_long',
+    'pass_rating',
+    'rush_att',
+    'rush_yds',
+    'rush_td',
+    'rush_long',
+    'targets',
+    'rec',
+    'rec_yds',
+    'rec_td',
+    'rec_long',
+    'fumbles',
+    'fumbles_lost'
+]
